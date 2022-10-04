cb65123c9bea5e7626c25c0c2998b3820692950d
VadimBilchich <vadimbilchich@rambler.ru>
2022-10-05 00:36:20 +0800

    Example 007 Magic

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int xa = 1, ya = 1,
+    xb = 1, yb = 1,
+    xc = 40, yc =- 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine(" + ");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine(" + ");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine(" + ");
