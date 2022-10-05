d94cf80613934522e0df45b2cebfe67804b0965f
VadimBilchich <vadimbilchich@rambler.ru>
2022-10-05 18:48:21 +0800

    Добавлено решение первой задачи

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("start");
+
+int value = 4;
+int result = value * value;
+Console.WriteLine(result);
+
+Console.WriteLine("end");
