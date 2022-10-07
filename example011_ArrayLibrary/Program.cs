2fc4db8333ee62db59a52fb710133e5b5908ce8b
VadimBilchich <vadimbilchich@rambler.ru>
2022-10-07 15:31:33 +0800

    Вхождение в массив с возвратом

--- example011_ArrayLibrary/Program.cs
+++ example011_ArrayLibrary/Program.cs
@@ -21,8 +21,38 @@ void PrintArray(int[] col)
         position ++;
     }
 }
+//int[] array = new int[10];
+//FillArray(array);
+//PrintArray(array);
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index ++;
+    }
+    return position;
+}
 
 int[] array = new int[10];
 
 FillArray(array);
+array[4] = 4;
+array[6] = 4;
 PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
+
+
+
+   
